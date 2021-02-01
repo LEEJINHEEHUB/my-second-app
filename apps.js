4eb2da627673551692120d86cc2d3a40e2f91579
LEEJINHEEHUB <jn3405@naver.com>
2021-02-01 16:48:28 +0900

    upload the first nodejs sample

---
+++ apps.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+// http://127.0.0.1:3000/
+app.get('/', (req, res) => res.send('Hello, Nodejs!'));
+// http://127.0.0.1:3000/welxome?name=jinhee
+app.get('/welcome', (req, res) => {
+    var user_name = req.param('name');
+    res.sendStatus('Hi there. Welcome to the Nodejs service [' + user_name + ']');
+});
+
+app.listen(port, () => console.log('Example app listening on port 3000')) // port를 사용하지 않으면 port번호 3000번을 사용
